92b84d8a350fffb6c0438f3d92442641df335f9b
Rakhi Agrahari <145806259+Rakhi-Agrahari123@users.noreply.github.com>
2025-06-21 21:26:28 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,3 +1,11 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+// import tailwindcss from '@tailwindcss/vite'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react(), tailwindcss()],
+// })
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
@@ -5,4 +13,8 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    chunkSizeWarningLimit: 2000,  // In KB, raises warning limit from 500 KB to 2000 KB (2MB)
+    outDir: 'dist',               // Optional: This is the default, can be omitted
+  },
 })
